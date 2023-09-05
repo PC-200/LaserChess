707d9dbd3973e43ff2269d549d580ee7bcc165eb
PC-200 <102894236+PC-200@users.noreply.github.com>
2023-09-05 23:20:53 +0300

    Added every piece and their way of movement

--- Assets/Code/Piece.cs
+++ Assets/Code/Piece.cs
@@ -1,13 +1,24 @@
 ﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using static UnityEditor.Experimental.GraphView.GraphView;
 
+
+[Serializable]
+public struct MovementDirection
+{
+    public Vector3 Direction;
+    public int Distance;
+}
 public class Piece : MonoBehaviour
 {
-    public int Hp;
-    public string Name;
+    public List<MovementDirection> Movement;
+    public List<MovementDirection> Attack;
+    public int MaxHealth;
+    public int AttackDamage;
+    public bool AoEDamage;
 
 }
 
